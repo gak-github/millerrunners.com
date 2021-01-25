3975b71c9e7928a3fa901f03c530a19639237e06
Asokumar Gurusamy <Asokumar Gurusamy>
2021-01-24 22:11:59 -0800

    dynamically updating the copywright year

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import styles from '../styles/footer.module.scss';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>Copyright &copy;2021 by Miller Runners. All rights reserved.</p>
+            <p>Copyright &copy;{new Date().getFullYear()} by Miller Runners. All rights reserved.</p>
         </footer>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Layout from '../components/Layout';
 
 const Home = () => {
+  const date = new Date();
+  const day = date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear();
   return (
     <Layout>
       <div className="description" />
+      <div className='time-updated'>
+        <span>Last updated: { new Date().getDate()}-{ new Date().getMonth()+1}-{ new Date().getFullYear() } </span>
+      </div>
       <div className='container'>
         <h2>This website is under construction....</h2>
       </div>     
